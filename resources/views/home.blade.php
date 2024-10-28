4f7b1f6da38a37d1e003b99a09565d86b9213e67
Ruben Leiva <rubenlrd@gmail.com>
2024-10-28 17:48:48 -0300

    Se agrega la Vista de inicio del blog home.blade.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es-ar">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel 11
+
+    </title>
+</head>
+<body>
+    <h1>Bienvenidos a la página principal</h1>
+</body>
+</html>
