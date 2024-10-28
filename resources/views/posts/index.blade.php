37c94c0cdc0404b232886515f070251d60809f14
Ruben Leiva <rubenlrd@gmail.com>
2024-10-28 17:50:15 -0300

    Se agregar las vistas del post, para mostrar, agregar y para listar

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es-ar">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel 11 | Posts</title>
+</head>
+<body>
+    <h1>Aqui se listaran todos los post</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,33 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use PhpParser\Node\Stmt\Else_;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);	
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::get('/posts/{post}', [PostController::class, 'show']);
+
+
+// Route::get('/', function () {
+//     return "Hola mundo";
+// });
+// Route::get('/posts', function () {
+//    return "Aqui se listaran todos los posts"; 
+// });
+
+// Route::get('/posts/create', function () {
+//     return "Aqui se mostrara el formulario para crear un nuevo post";
+// });
+
+// Route::get('/posts/{post}', function ($post) {
+//     return "Aqui se mostrara el post {$post}";
+// });
+
+
+
+// Route::get('/posts/category', function ($post, $category) {
+//     return "Aqui se mostraran todos los posts {$post} de la categoria {$catego}";
+// });
