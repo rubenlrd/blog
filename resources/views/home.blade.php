1dc571adf849bfb75abca454acc2667003da0a3c
Ruben Leiva <rubenlrd@gmail.com>
2024-10-29 23:00:55 -0300

    Archivo alert de components
    Se creo archivo de componente reutilizable

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,26 @@
 <!DOCTYPE html>
 <html lang="es-ar">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel 11
-
-    </title>
+    <title>Laravel 11</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
+
 <body>
-    <h1>Bienvenidos a la página principal</h1>
+    <div class="max-w-4xl mx-auto px-4">
+        {{--<h1>Bienvenidos a la página principal</h1>--}}
+        <div class="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
+            <x-alert>
+                <x-slop name="title">
+                    Titulo de la Alerta
+                </x-slo>
+                Contenido de la alerta.
+            </x-alert>
+        </div>
+    </div>
 </body>
+
 </html>
